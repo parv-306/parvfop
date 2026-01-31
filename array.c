bb278229b81f3d906eb06966935dd289b84915b4
parv-306 <parvjain835@gmail.com>
2026-01-31 05:22:08 +0000

    new files

---
+++ array.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+ int  main() {
+
+    int a[10];
+    printf("enter arrary elements");
+    int i;
+    for(i=0;i<= 9; i++)
+    {
+    scanf("%d",&a[i]);
+    }
+    for(i=0;i<=9; i++) {
+        printf("%d",a[i]);
+
+    }
+    return 0;
+ } 
+
+
+     
+
+ 
---
+++ array2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main() {
+    int a[3];
+    print("enter elments:/n");
+    int i;
+    for(i=0;i<=2; i++)
+    
+
+}
+
