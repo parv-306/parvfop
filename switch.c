a7ba0f0f6c613ea00518beaebf6f02d8f4d4a9b2
parv-306 <parvjain835@gmail.com>
2026-02-05 06:24:51 +0000

    newfile

--- switch.c
+++ switch.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdio.h>
 int main() {
 int ch;
 int num1;
@@ -31,6 +32,21 @@ break;
 
 case(4):
     printf("division of two number is ;%d",num1/num2);
+break;
+
+case(5):
+    printf("%f raised to the power %f is: %f is: %f\n",num1, num2);
+break;
+
+case(6):
+   printf("factorial of %f is: %f is: %f\n", ((int)num1),num1);
+break;
+
+case(7):
+   exit(0);
+break;
+
+default :
 break;
 
 
