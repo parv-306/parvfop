fa493889484ddbeaacd2215122ef13ac35de8f95
parv-306 <parvjain835@gmail.com>
2026-02-12 06:13:06 +0000

    new file

---
+++ gcd.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main() {
+
+int num1;
+int num2;
+
+int old_div=1;
+int new_div;
+
+printf("enter the first number:");
+scanf("%d", &num1);
+ 
+printf("enter the second number:");
+scanf("%d", &num2);
+
+for(old_div;old_div<num1 && old_div<num2;old_div++){
+    if(num1%old_div==0 && num2%old_div==0) {
+        new_div = old_div;
+
+    
+    }
+}
+printf("the gcd of the number is %d", new_div);
+    return 0;
+}
